81c93d95b962905de1117182bea832a343aff083
Ilya Obukhov <ilya.obukhov.bs@gmail.com>
2024-11-28 10:06:44 +0100

    Add test cases for the reverse words in a string problem
    Introduces a new file for JUnit tests to validate the solution for reversing words in a string. Tests various scenarios including trimming excess whitespace and reversing the order of words.

---
+++ src/test/kotlin/I0155ReverseWordsInAString.kt
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Nested
+import kotlin.test.Test
+
+
+/**
+ * https://leetcode.com/problems/two-sum/description/
+ */
+class I0155ReverseWordsInAString {
+    data class Case(
+        val input: String,
+        val output: String,
+    )
+
+    @Nested
+    inner class Solution : AproblemTest<Case, (String) -> String> {
+
+        override val cases: List<Case> = listOf(
+            Case("the sky is blue", "blue is sky the"),
+            Case("  hello world  ", "world hello"),
+            Case("a good   example", "example good a"),
+        )
+
+        override val solutions: List<Pair<String, (String) -> String>> = listOf(
+            "solution1" to ::solution1,
+        )
+
+        override fun Case.check(solution: (String) -> String): Pair<Boolean, Any> {
+            val result = solution(input)
+            val isCorrect = result == output
+            return isCorrect to result
+        }
+
+        @Test
+        fun test() = check()
+
+        private fun solution1(s: String): String {
+            return s.split(" ").mapNotNull {
+                if (it.isBlank()) null else it.trim()
+            }.reversed().joinToString(" ")
+        }
+
+    }
+}
