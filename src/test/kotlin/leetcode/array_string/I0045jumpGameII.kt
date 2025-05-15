0eabcd1cacbc6010ee8f223a2013a3a4b5e7b92c
Ilya Obukhov <ilya.obukhov.bs@gmail.com>
2025-05-15 10:09:34 +0200

    Add solution and tests for Jump Game II problem
    Implemented a solution and corresponding unit tests for the "Jump Game II" problem. The solution uses a BFS-like approach with memoization to determine the minimum jumps required. Added diverse test cases to verify correctness and edge case handling.

---
+++ src/test/kotlin/leetcode/array_string/I0045jumpGameII.kt
@@ -0,0 +1,64 @@
+package leetcode.array_string
+
+import leetcode.AproblemTest
+import leetcode.utils.ArrayUtils.toIntArray
+import org.junit.jupiter.api.Nested
+import kotlin.test.Test
+
+private typealias I0045 = (IntArray) -> Int
+
+class I0045jumpGameII {
+
+    data class Case(
+        val nums: List<Int>,
+        val output: Int,
+    )
+
+    val prepareCase = { n1: String, r: Int ->
+        Case(n1.toIntArray().toList(), r)
+    }
+
+
+    @Nested
+    inner class Solution : AproblemTest<Case, I0045> {
+        override val cases: List<Case> = listOf(
+            prepareCase("[2,3,1,1,4]", 2),
+            prepareCase("[2,3,0,1,4]", 2),
+            prepareCase("[9,8,2,2,0,2,2,0,4,1,5,7,9,6,6,0,6,5,0,5]", 3)
+        )
+        override val solutions: List<Pair<String, I0045>> = listOf(
+            ::solution1.name to ::solution1,
+        )
+
+        override fun Case.check(solution: I0045): Pair<Boolean, Any> {
+            val result = solution(nums.toIntArray())
+            return (result == output) to result
+        }
+
+        @Test
+        fun test() = check()
+
+        /**
+         * MLE
+         */
+        fun solution1(nums: IntArray): Int {
+            if (nums.size == 1) return 0
+            val memo = MutableList<Int?>(nums.size) { null }
+            memo[0] = 0
+
+            val queue = ArrayDeque<Int>()
+            queue.add(0)
+            while (queue.isNotEmpty()) {
+                val idx = queue.removeFirst()
+                for (jump in nums[idx] downTo 1) {
+                    val target = idx + jump //2
+                    if (target >= nums.lastIndex) return (memo[idx] ?: 0) + 1 //if jump over the last index, return
+                    memo[target] = minOf((memo[idx] ?: 0) + 1, memo[target] ?: Int.MAX_VALUE)
+                    queue.add(target)
+                }
+            }
+            return 0
+        }
+
+    }
+}
